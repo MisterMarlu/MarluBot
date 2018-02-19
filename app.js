56648d6dc48f5d1286c4d273760c52873cfdce54
Luca Braun <l.braun@rto.de>
2018-02-19 14:32:37 +0100

    Add translation functionality

--- app.js
+++ app.js
@@ -1,25 +1,9 @@
-if (typeof NO_DEBUG === 'undefined') {
-  /**
-   * Define DEBUG for debugging.
-   */
-  Object.defineProperty(global, 'NO_DEBUG', {
-    get: function () {
-      let noDebug = false;
-
-      for (let i = 0; i < process.argv.length; i += 1) {
-        if (process.argv[i] === 'noDebug') noDebug = true;
-      }
-
-      return noDebug;
-    }
-  });
-}
+require('./lib/global');
 
 const Discord = require('discord.js'),
   config = require('./config'),
   tokens = require('./tokens'),
   {Commands} = require('./lib/commands'),
-  {getBotInfo} = require('./lib/info'),
   bot = new Discord.Client();
 
 // Executes when the bot is ready.
@@ -31,15 +15,6 @@ bot.on('ready', () => {
 bot.on('message', message => {
   // So the bot doesn't reply to iteself
   if (message.author.bot) return;
-
-  let mentionedUsers = message.mentions.users.array();
-  for (let i = 0; i < mentionedUsers.length; i += 1) {
-    let user = mentionedUsers[i];
-    if (user.username === bot.user.username && user.bot) {
-      // message.reply(getBotInfo(bot));
-      // return;
-    }
-  }
 
   // Check if the message starts with the prefix trigger
   if (message.content.indexOf(config.prefix) !== 0) return;
---
+++ lang/en.js
@@ -0,0 +1,24 @@
+let en = {
+  // Descriptions for commands.
+  descTest: 'Test if the bot is awake.',
+  descGetCommands: 'Get a list of all commands with it\'s description.',
+  descLolElo: 'Get the current rank information of a summoner. Argument one must be the summoner name.',
+  descInfo: 'Get the current information of this bot.',
+  descDebug: 'Just for development.',
+
+  // Commands
+  commandTest: 'I woke up and I\'m alive.',
+  eloStart: 'Ranked stats of %s (lvl %s)',
+  unranked: 'Unranked',
+  eloStat: '%s wins, %s losses, %s LP',
+  getCommands: 'List of available commands:',
+  botInfo: 'Information about this bot:',
+  update: 'Last update: %s',
+
+  // Warnings.
+  noCommand: 'There is no command called "%s". Type "?get-commands" to get a list of commands with it\'s description.',
+  debugNoCommand: 'No command available called "%s".',
+  eloError: 'There is an issue, maybe a spelling in the summoner name :(',
+};
+
+exports.en = en;
